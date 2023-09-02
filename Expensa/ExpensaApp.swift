ce9e5ccf8e69cc9f3e0a0e2250209a5d4935346a
Minoli Fernando <minofdo97@gmail.com>
2023-09-02 21:17:59 +0530

    initial commit

---
+++ Expensa/ExpensaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensaApp.swift
+//  Expensa
+//
+//  Created by Minoli Fernando on 2023-09-02.
+//
+
+import SwiftUI
+
+@main
+struct ExpensaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
